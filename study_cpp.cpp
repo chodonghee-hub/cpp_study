38f0816a621a7d8d760847e54af9a6926417f090
chodonghee-hub <timandsunny@naver.com>
2022-01-06 17:37:51 +0900

    return object copy

--- study_cpp.cpp
+++ study_cpp.cpp
@@ -3,24 +3,29 @@ using namespace std;
 
 class SoSimple {
 private:
-	int num1, num2;
+	int num;
 public:
-	SoSimple(int n1, int n2) : num1(n1), num2(n2) { this->ShowSimpleData(); }
-	SoSimple(SoSimple &copy) : num1(copy.num1), num2(copy.num2) {
-		cout << "*** Called SoSimple(SoSImple &copy)" << endl;
-		this->ShowSimpleData();
+	SoSimple(int n) :num(n) {}
+	SoSimple(const SoSimple& copy) : num(copy.num) {
+		cout << "Called SoSimple(const SoSimple& copy)" << endl;
 	}
-	void ShowSimpleData() {
-		cout << "*** now object : " << this << endl;
-		cout << "*** num1 num2 : " << num1 << " " << num2 << endl << endl;
+	SoSimple& AddNum(int n) {
+		num += n;
+		return *this;
+	}
+	void ShowData() {
+		cout << " num : " << num << endl;
 	}
 };
 
-int main(void) {
-	cout << "생성 및 초기화 직전" << endl;
-	SoSimple s1(15, 30);
+SoSimple SoSimpleFuncObj(SoSimple ob) {
+	cout << "return 이전 " << endl;
+	return ob;
+}
 
-	cout << "생성 및 초기화 직후" << endl;
-	SoSimple s2(s1);
+int main(void) {
+	SoSimple obj(7);
+	SoSimpleFuncObj(obj).AddNum(30).ShowData();
+	obj.ShowData();
 	return 0;
 }
